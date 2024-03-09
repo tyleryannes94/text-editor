15cb29acce32ee3311dff1b556609d0e2f560d0e
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-09 15:04:23 -0600

    completed the webpack.config.js file required in the react client app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,63 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html', 
+        filename: 'index.html',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js', 
+        swDest: 'service-worker.js', 
+      }),
+      new WebpackPwaManifest({
+        name: 'Your App Name',
+        short_name: 'App',
+        description: 'Your app description',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', 
+        icons: [
+          {
+            src: path.resolve('src/images/log.png'), 
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
       ],
+    },
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
     },
   };
 };
