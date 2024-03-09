4c00139a83e09c4959025bc3b47b3ff7bb1aae54
tyleryannes94 <tyler.yannes@gmail.com>
2024-03-09 15:25:18 -0600

    added the register routes for asset caching and a function call offlineFallBack.

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,39 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script' || request.destination === 'font',
+  new CacheFirst({
+    cacheName: 'static-resources',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 50, 
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        maxEntries: 60, 
+      }),
+    ],
+  })
+);
+
+offlineFallback({
+  pageFallback: '/offline.html',
+  imageFallback: '/images/offline.png',
+});
